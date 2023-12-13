0a28837b410deb4d635f24ed7bf9839873fc850d
abdoulaye yatera <yatera.a@outlook.com>
2023-12-13 02:24:15 +0100

    update version

--- systemd-service-watcher.sh
+++ systemd-service-watcher.sh
@@ -14,7 +14,7 @@ top_dir=${my_path%/*}
 my_real_name=${my_path##*/}
 
 script_name="Systemd Service Watcher"
-script_version="3.0.1"
+script_version="3.0.2"
 installed_path="/opt/systemd-service-watcher"
 installed_script_path="${installed_path}/systemd-service-watcher.sh"
 installed_conf_path="${installed_path}/systemd-service-watcher.conf"
